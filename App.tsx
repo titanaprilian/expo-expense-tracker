1eb9420f44e5374b20ce5b4680ed1ba3b5e4d5c6
titanaprilian <titanaprilian73@gmail.com>
2026-04-02 21:25:38 +0700

    Add data persistence with AsyncStorage

--- App.tsx
+++ App.tsx
@@ -1,6 +1,19 @@
 import "./global.css";
 import AppNavigator from './src/navigation/AppNavigator';
+import { useEffect } from 'react';
+import { useExpenseStore } from './src/features/expense/store/useExpenseStore';
 
 export default function App() {
+  const hydrate = useExpenseStore((state) => state.hydrate);
+  const hydrated = useExpenseStore((state) => state.hydrated);
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
+  if (!hydrated) {
+    return null;
+  }
+
   return <AppNavigator />;
 }
